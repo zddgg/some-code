0c3b19494ef6a9e72b4e636b3bd3a9bbfa7cb04b
zddgg <liuwenliang001@163.com>
2023-03-17 22:55:53 +0800

    feat: lc72

---
+++ src/main/java/com/zddgg/study/MinDistance.java
@@ -0,0 +1,31 @@
+package com.zddgg.study;
+
+/**
+ * lc72. 编辑距离
+ */
+public class MinDistance {
+    public static void main(String[] args) {
+        String word1 = "intention", word2 = "execution";
+        System.out.println(minDistance(word1, word2));
+    }
+
+    public static int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i < dp[0].length; i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[i].length; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                }
+            }
+        }
+        return dp[dp.length - 1][dp[0].length - 1];
+    }
+}
