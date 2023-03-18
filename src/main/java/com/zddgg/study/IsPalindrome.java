841ece5d7b442ff48d5b9427f74d7389a09321b1
zddgg <liuwenliang001@163.com>
2023-03-18 23:24:34 +0800

    feat: lc234

---
+++ src/main/java/com/zddgg/study/IsPalindrome.java
@@ -0,0 +1,43 @@
+package com.zddgg.study;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * lc234. 回文链表
+ */
+public class IsPalindrome {
+
+    public static void main(String[] args) {
+        ListNode head = null;
+        ListNode tail = null;
+        int[] nums = {1, 2, 3, 1};
+        for (int num : nums) {
+            if (head == null) {
+                head = tail = new ListNode(num);
+            } else {
+                tail.next = new ListNode(num);
+                tail = tail.next;
+            }
+        }
+        System.out.println(isPalindrome(head));
+    }
+
+    public static boolean isPalindrome(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+        int start = 0;
+        int end = list.size() - 1;
+        while (end >= start) {
+            if (list.get(start) != list.get(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
